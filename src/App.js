e8c96683bb3902d2c2743f20503b013638b20945
anadolacio <anadolacio@hotmail.com>
2022-11-27 17:56:43 -0300

    resolução requisito 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <SolarSystem />
+        <Title />
+      </div>
+
     );
   }
 }
